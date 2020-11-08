24d858041ede833dc46f780fcc527fdf3bd47092
ltery <74140235+ltery@users.noreply.github.com>
2020-11-08 20:47:53 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html> 
+<html> 
+  
+<head> 
+    <title>door step grocery</title> 
+    <style type="text/css"> 
+        html { 
+            overflow: auto; 
+        } 
+          
+        html, 
+        body, 
+        div, 
+        iframe { 
+            margin: 0px; 
+            padding: 0px; 
+            height: 100%; 
+            border: none; 
+        } 
+          
+        iframe { 
+            display: block; 
+            width: 100%; 
+            border: none; 
+            overflow-y: auto; 
+            overflow-x: hidden; 
+        } 
+    </style> 
+</head> 
+  
+<body> 
+    <iframe src="https://www.yamibuy.com/en/activity/pgAdWy98G?track=search-default-0-11.11Category%2012%25%2520OFF/"
+            frameborder="0" 
+            marginheight="0" 
+            marginwidth="0" 
+            width="100%" 
+            height="100%" 
+            scrolling="auto"> 
+  </iframe> 
+  
+</body> 
+  
+</html> 
